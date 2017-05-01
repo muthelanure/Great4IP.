99efdcd3dd3cf576d2fa659eea5041999e865930
billpowersinaga <billpower11@gmail.com>
2017-05-01 23:09:04 +0700

    aku ngyoba push

--- app/src/main/java/com/example/a6sigma/great4ip/message.java
+++ app/src/main/java/com/example/a6sigma/great4ip/message.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class message extends AppCompatActivity {
 
+    private String cinta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
